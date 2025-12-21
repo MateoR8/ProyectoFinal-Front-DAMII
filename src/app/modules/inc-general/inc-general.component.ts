60d8352e9bfe58b76d2548b520a8e65ba5a536cf
Mateo Jheremy <mateoquispe08@hotmail.com>
2025-12-21 11:08:45 -0500

    Duracion de minutos agregado

--- src/app/modules/inc-general/inc-general.component.ts
+++ src/app/modules/inc-general/inc-general.component.ts
@@ -21,8 +21,35 @@ export class IncGeneralComponent implements OnInit {
         const numA = Number(a.codigoIncidencia.split('-')[1]);
         const numB = Number(b.codigoIncidencia.split('-')[1]);
         return numA - numB;
-      });
+      }).map(inc => ({
+        ... inc,
+        duracionMinutos : this.calcularDuracion(inc.fechaCreacion, inc.fechaFinalizacion)
+      }));
     });
+  }
+
+  calcularDuracion(fechaInicio: string, fechaFin: string): string {
+    if (!fechaFin) return 'Aún no finaliza';
+    const inicio = this.parseFecha(fechaInicio);
+    const fin = this.parseFecha(fechaFin);
+    if (!inicio || !fin) return 'Formato de fecha inválido';
+    const diffMs = fin.getTime() - inicio.getTime();
+    const diffMin = Math.round(diffMs / 60000);
+
+    return `${diffMin} min`;
+  }
+
+  parseFecha(fechaStr: string | undefined | null): Date | null {
+    if (!fechaStr) return null;
+    const partes = fechaStr.split(' ');
+    if (partes.length !== 2) return null;
+    const [fechaPart, horaPart] = partes;
+    const fechaNums = fechaPart.split('/').map(Number);
+    const horaNums = horaPart.split(':').map(Number);
+    if (fechaNums.length !== 3 || horaNums.length !== 2) return null;
+    const [dd, mm, yyyy] = fechaNums;
+    const [hh, min] = horaNums;
+    return new Date(yyyy, mm - 1, dd, hh, min);
   }
 
   abrirVisualizar(incidencia: any) {
